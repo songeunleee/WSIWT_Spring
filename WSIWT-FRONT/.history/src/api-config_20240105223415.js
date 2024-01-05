41faaac89e0caa054a0b18421257febbe1081bf2
Songeun <songsilver.lee@gmail.com>
2024-01-06 03:01:46 +0900

    deploy

--- WSIWT-FRONT/.history/src/api-config_20240105223415.js
+++ WSIWT-FRONT/.history/src/api-config_20240105223415.js
@@ -2,6 +2,6 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-backendHost = "http://demo22.us-west-2.elasticbeanstalk.com";
+backendHost = "http://demo22.us-west-2.elasticbeanstalk.com/";
 
 export const API_BASE_URL = `${backendHost}`;
