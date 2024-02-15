2d5c25ee89afa08cdb10bde22156e587185ec09a
Songeun <songsilver.lee@gmail.com>
2024-02-15 23:12:55 +0900

    cors

--- WSIWT_BACK/src/main/java/com/example/wsiwt_back/config/WebMvcConfig.java
+++ WSIWT_BACK/src/main/java/com/example/wsiwt_back/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/","https://hilarious-liger-6a4c09.netlify.app/")
+                .allowedOrigins("http://localhost:3000/","https://hilarious-liger-6a4c09.netlify.app/","https://front.wsiwt.store/")
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
